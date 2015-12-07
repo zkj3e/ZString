44f1100788462bbc2fbdd6b9a285cff327881084
zkj3e <zkj3e@163.com>
2015-12-07 23:43:42 +0800

    add fun
    增加函数声明

--- src/ZString.h
+++ src/ZString.h
@@ -10,19 +10,19 @@ public:
 	typedef unsigned int SIZE_T;
 	#define npos -1
 
-	SIZE_T length();
+	SIZE_T length() const;
 
-	bool isEmpty();
+	bool isEmpty() const;
 
-	ZString reverse();
+	ZString reverse() const;
 
 	void clear();
 
-	ZString &chop(int n);
+	ZString chop(int n) const;
 
-	ZString trimLeft();
-	ZString trimRight();
-	ZString trim();
+	ZString trimLeft() const;
+	ZString trimRight() const;
+	ZString trim() const;
 
 	ZString & append(char c);
 	ZString & append(const ZString &str);
@@ -34,14 +34,14 @@ public:
 	const char at(int i) const;
 	const char operator[](int i) const;
 
-	int indexOf(char c,int iFrom = 0);
-	int indexOf(const ZString &str,int iFrom = 0);
+	int indexOf(char c,int iFrom = 0) const;
+	int indexOf(const ZString &str,int iFrom = 0) const;
 
-	int lastIndexOf(char c,int iFrom = -1);
-	int lastIndexOf(const ZString &str,int iFrom = -1);
+	int lastIndexOf(char c,int iFrom = -1) const;
+	int lastIndexOf(const ZString &str,int iFrom = -1) const;
 	
-	bool contains(char c);
-	bool contains(const ZString &str);
+	bool contains(char c) const;
+	bool contains(const ZString &str) const;
 	
 	ZString & insert(int iPos,char c);
 	ZString & insert(int iPos,const ZString &str);
@@ -50,9 +50,9 @@ public:
 	ZString & replace(char cSrc,char cDst);
 	ZString & replace(const ZString &strSrc,const ZString &strDst);
 
-	ZString left(int iPos);
-	ZString right(int iPos);
-	ZString mid(int iPos,int iLen = -1);
+	ZString left(int iPos) const;
+	ZString right(int iPos) const;
+	ZString mid(int iPos,int iLen = -1) const;
 
 	ZString & operator=(const ZString &strOther);
 	ZString & operator=(const char *cStr);
@@ -70,8 +70,43 @@ public:
 	char *data();
 	const char *data() const;
 
-	
+	std::string toStdString() const;
+	short toShort(bool *ok = 0,int iBase = 10) const;
+	int toInt(bool *ok = 0,int iBase = 10) const;
+	long toLong(bool *ok = 0,int iBase = 10) const;
+	long long toLongLong(bool *ok = 0,int iBase = 10) const;
 
+	unsigned short toUShort(bool *ok = 0,int iBase = 10) const;
+	unsigned int toUInt(bool *ok = 0,int iBase = 10) const;
+	unsigned long toULong(bool *ok = 0,int iBase = 10) const;
+	unsigned long long toULongLong(bool *ok = 0,int iBase = 10) const;
+	
+	float toFloat(bool *ok = 0) const;
+	double toDouble(bool *ok = 0) const;
+	
+	static ZString fromStdString(const std::string &str);
+	static ZString fromShort(short num,int iBase = 10);
+	static ZString fromInt(int num,int iBase = 10);
+	static ZString fromLong(long num,int iBase = 10);
+	static ZString fromLongLong(long long num,int iBase = 10);
+	static ZString fromUShort(unsigned short num,int iBase = 10);
+	static ZString fromUInt(unsigned int num,int iBase = 10);
+	static ZString fromULong(unsigned long num,int iBase = 10);
+	static ZString fromULongLong(unsigned long long num,int iBase = 10);
+	static ZString fromFloat(float num);
+	static ZString fromDouble(double num);
+
+	enum SplitBehavior
+	{
+		KeepEmptyParts = 0,
+		SkipEmptyParts = 1,
+	};
+
+	std::list<ZString> split(char sp,SplitBehavior behavior = KeepEmptyParts) const;
+	std::list<ZString> split(const ZString &sp,SplitBehavior behavior = KeepEmptyParts) const;
+
+	ostream &operator<<(ostream &output) const;
+	istream &operator>>(istream &input) const;
 
 private:
 	char *m_pData;
